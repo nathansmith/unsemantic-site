1e005698c2ed1025cab8f1b95c0b4190eab2e6d8
Nathan Smith <nathan@sonspring.com>
2013-02-03 19:38:32 -0600

    Tweaks for IE7.

--- views/view_helpers.rb
+++ views/view_helpers.rb
@@ -1,8 +1,3 @@
-#
-# Methods placed in this module can be used inside of any view.
-# View helpers allow you to encapsalate complex logic and keep your
-# views pristine.
-#
 module ViewHelpers
   def html
     if @flat_html == true
